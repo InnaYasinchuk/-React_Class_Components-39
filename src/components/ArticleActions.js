f298dd1d406599838ebe06eacb62a1d3b6de6a6c
Inna Yasynchuk <i_bybyk@ukr.net>
2023-02-24 13:46:47 +0200

    changing the language of the app, adding user data to localStorage

---
+++ src/components/ArticleActions.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import LangContext from "./lang-context";
+
+class ArticleActions extends Component {
+  render() {
+    return (
+      <LangContext.Consumer>
+        {(context) => {
+          return (
+            <div className="article__actions">
+              <button className="article__btn">{context.action}</button>
+            </div>
+          );
+        }}
+      </LangContext.Consumer>
+    );
+  }
+}
+
+export default ArticleActions;
